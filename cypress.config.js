75590271321959fac578c079cb320af9ed224ded
daniel.januario <daniel.januario@luizalabs.com>
2025-02-03 20:10:38 -0300

    Run the same test a hundred times

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
   e2e: {
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
